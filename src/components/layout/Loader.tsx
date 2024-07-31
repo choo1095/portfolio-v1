05768837ea1ff867c235de2e280bde95359f2be0
ennichoo <ennichoo@mobius-digital.com>
2024-07-31 23:23:06 +0800

    loader - code cleanup

--- src/components/layout/Loader.tsx
+++ src/components/layout/Loader.tsx
@@ -3,8 +3,6 @@ import React, { useEffect, useState } from "react";
 import { animate, motion, stagger, useAnimate, useSpring } from "framer-motion";
 import { useInitializationContext } from "@/contexts/useInitializationContext";
 import { sleep } from "@/lib/utils";
-import Me from "../me/Me";
-import strings from "@/constants/strings";
 
 const initial = {
   opacity: 0,
@@ -14,166 +12,6 @@ const initial = {
 interface Props {
   className?: string;
 }
-
-// const LOADER_DURATION_SECONDS = 2;
-
-// const loaderTextClass = "text-2xl font-sans-serif font-semibold md:text-3xl";
-// const percentageTextClass = "text-lg font-sans font-light md:text-xl";
-
-// const initialFadeClass = "opacity-0 translate-y-1/2";
-
-// const useLoaderAnimation = () => {
-//   const [scope, animate] = useAnimate();
-
-//   const { setLoaderComplete } = useInitializationContext();
-
-//   useEffect(() => {
-//     const _animate = async () => {
-//       // fade grey text in, then reveal red text
-//       await animate([
-//         [
-//           ".loading_background div",
-//           { transform: "translateY(0)", opacity: 1 },
-//           { delay: stagger(0.07), ease: "easeInOut" },
-//         ],
-//         [
-//           ".loading_foreground",
-//           { width: "100%", overflow: "clip" },
-//           {
-//             duration: LOADER_DURATION_SECONDS,
-//             delay: stagger(0.8),
-//             ease: [0, 0.43, 0.67, 0.9],
-//           },
-//         ],
-//       ]);
-
-//       // set the properties required for the next animation to continue
-//       await animate([
-//         [".loading_background", { visibility: "hidden" }],
-//         [".loading_foreground", { overflow: "visible" }],
-//       ]);
-
-//       // fade out red text
-//       await animate([
-//         [
-//           ".loading_foreground div",
-//           { transform: "translateY(-50%)", opacity: 0 },
-//           { delay: stagger(0.05), ease: "easeInOut" },
-//         ],
-//       ]);
-
-//       await sleep(300);
-
-//       setLoaderComplete(true);
-//     };
-
-//     _animate();
-//   }, []);
-
-//   return scope;
-// };
-
-// const LoaderPercentage = () => {
-//   const [number, setNumber] = useState("0");
-
-//   useEffect(() => {
-//     const updateNumber = animate(0, 100, {
-//       duration: LOADER_DURATION_SECONDS,
-//       ease: "easeInOut",
-//       delay: 2,
-//       onUpdate: (latest: number) => setNumber(latest.toFixed(0)),
-//     });
-
-//     return () => {
-//       updateNumber.cancel();
-//     };
-//   }, []);
-
-//   return (
-//     <div className="relative items-end">
-//       <span className="loading_background flex text-stone-400 animate-pulse">
-//         <div className={clsx(percentageTextClass, initialFadeClass)}>.</div>
-//         <div className={clsx(percentageTextClass, initialFadeClass)}>.</div>
-//         <div className={clsx(percentageTextClass, initialFadeClass)}>.</div>
-//         <div className={clsx(percentageTextClass, initialFadeClass)}>
-//           {number[0]}
-//         </div>
-//         <div className={clsx(percentageTextClass, initialFadeClass)}>
-//           {number[1]}
-//         </div>
-//         <div className={clsx(percentageTextClass, initialFadeClass)}>
-//           {number[2]}
-//         </div>
-//         <div className={clsx(percentageTextClass, initialFadeClass)}>%</div>
-//       </span>
-//       {/* <motion.span */}
-//       <motion.span
-//         className={clsx(
-//           percentageTextClass,
-//           "loading_foreground flex absolute left-0 bottom-0 text-primary-900"
-//         )}
-//         initial={{
-//           width: 0,
-//           overflow: "clip",
-//         }}
-//       >
-//         <div className={clsx(percentageTextClass)}>.</div>
-//         <div className={clsx(percentageTextClass)}>.</div>
-//         <div className={clsx(percentageTextClass)}>.</div>
-//         <div className={clsx(percentageTextClass)}>{number[0]}</div>
-//         <div className={clsx(percentageTextClass)}>{number[1]}</div>
-//         <div className={clsx(percentageTextClass)}>{number[2]}</div>
-//         <div className={clsx(percentageTextClass)}>%</div>
-//       </motion.span>
-//     </div>
-//   );
-// };
-
-// const Loader = (props: Props) => {
-//   const scope = useLoaderAnimation();
-
-//   return (
-//     <div
-//       ref={scope}
-//       className={clsx(
-//         props.className,
-//         "screen_container section_padding fixed top-0 left-0 bottom-0 right-0 z-50 flex items-end justify-start"
-//       )}
-//     >
-//       <div className="relative flex gap-1 items-end">
-//         <span className="loading_background flex text-stone-400 animate-pulse">
-//           <div className={clsx(loaderTextClass, initialFadeClass)}>L</div>
-//           <div className={clsx(loaderTextClass, initialFadeClass)}>o</div>
-//           <div className={clsx(loaderTextClass, initialFadeClass)}>a</div>
-//           <div className={clsx(loaderTextClass, initialFadeClass)}>d</div>
-//           <div className={clsx(loaderTextClass, initialFadeClass)}>i</div>
-//           <div className={clsx(loaderTextClass, initialFadeClass)}>n</div>
-//           <div className={clsx(loaderTextClass, initialFadeClass)}>g</div>
-//         </span>
-
-//         <motion.span
-//           className={clsx(
-//             "loading_foreground flex absolute left-0 bottom-0 text-primary-900"
-//           )}
-//           initial={{
-//             width: 0,
-//             overflow: "clip",
-//           }}
-//         >
-//           <div className={clsx(loaderTextClass)}>L</div>
-//           <div className={clsx(loaderTextClass)}>o</div>
-//           <div className={clsx(loaderTextClass)}>a</div>
-//           <div className={clsx(loaderTextClass)}>d</div>
-//           <div className={clsx(loaderTextClass)}>i</div>
-//           <div className={clsx(loaderTextClass)}>n</div>
-//           <div className={clsx(loaderTextClass)}>g</div>
-//         </motion.span>
-
-//         <LoaderPercentage></LoaderPercentage>
-//       </div>
-//     </div>
-//   );
-// };
 
 const useFadeSequenceAnimation = () => {
   const { setLoaderComplete } = useInitializationContext();
@@ -327,35 +165,35 @@ const LoaderContents = (props: Props) => {
   );
 };
 
-const Curtains = (props: Props) => {
-  return (
-    <div
-      className={clsx(
-        "absolute top-0 left-0 right-0 bottom-0 flex flex-col",
-        props.className
-      )}
-    >
-      <motion.div
-        className="bg-stone-700 w-full h-full"
-        initial={{ transform: "translateY(100%) " }}
-        animate={{ transform: "translateY(-100%)" }}
-        transition={{
-          ease: "easeOut",
-          duration: 0.5,
-          delay: 1,
-        }}
-      ></motion.div>
-      {/* <div className="w-full h-px bg-stone-300"></div>
-      <motion.div
-        className="bg-stone-700 w-full h-1/2"
-        animate={{ transform: "translateY(100%)" }}
-        transition={{
-          duration: 1,
-        }}
-      ></motion.div> */}
-    </div>
-  );
-};
+// const Curtains = (props: Props) => {
+//   return (
+//     <div
+//       className={clsx(
+//         "absolute top-0 left-0 right-0 bottom-0 flex flex-col",
+//         props.className
+//       )}
+//     >
+//       <motion.div
+//         className="bg-stone-700 w-full h-full"
+//         initial={{ transform: "translateY(100%) " }}
+//         animate={{ transform: "translateY(-100%)" }}
+//         transition={{
+//           ease: "easeOut",
+//           duration: 0.5,
+//           delay: 4.5,
+//         }}
+//       ></motion.div>
+//       {/* <div className="w-full h-px bg-stone-300"></div>
+//       <motion.div
+//         className="bg-stone-700 w-full h-1/2"
+//         animate={{ transform: "translateY(100%)" }}
+//         transition={{
+//           duration: 1,
+//         }}
+//       ></motion.div> */}
+//     </div>
+//   );
+// };
 
 const Loader = (props: Props) => {
   return (
