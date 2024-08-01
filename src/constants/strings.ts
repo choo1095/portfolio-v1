ed097c088ce021849cd58a7dfd14137c8adf8f72
ennichoo <ennichoo@mobius-digital.com>
2024-08-01 12:20:56 +0800

    updated about me contents

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -2,14 +2,15 @@ const strings = {
   me: {
     name: "Choo En-Ni",
     title: "Full-stack Developer",
-    description: "A developer from Malaysia. I make mobile apps and websites.",
+    description:
+      "Developing mobile apps and websites designed with your needs at the center",
   },
   about_me: {
     header: "About Me",
     contents: [
-      "I began my journey into accounting during my high school and A-level years, which continued into my undergraduate studies. Although I realized early on that it wasn't the right fit for me, I persevered and completed my degree with satisfactory grades.",
-      "I then decided to switch gears and pursued a second degree in programming. This decision opened up new opportunities, and for the past three years, I've been thriving in a fast-paced environment at a startup company. Here, I've honed my skills in developing user-friendly mobile applications and dynamic websites, creating seamless user experiences that drive engagement and satisfaction.",
-      "Along the way, I've been experimenting with and exploring different programming languages, constantly broadening my technical knowledge and adapting to new challenges.",
+      "I've been a mobile apps and web developer for the past three years, but my journey began elsewhere. I was an accounting student during my high school years, which continued into my undergraduate studies. I realized early on that it wasn't the right path for me and eventually pivoted to a career in development after persevering through and completing my accounting degree.",
+      "I began my tech career with native iOS app development using Swift as a mobile application developer. I then transitioned to Flutter to meet the need for developing Android apps in addition to iOS. This shift allowed me to create hybrid apps for both platforms. While working with Flutter, I gradually delved into front-end development using Vue and Nuxt, expanding my experience with dynamic web design and user interfaces. This experience eventually led me to embrace full-stack development, where I now build comprehensive applications using Laravel, integrating both front-end and back-end technologies seamlessly.",
+      "Throughout my journey, I've been experimenting with and exploring different programming languages, constantly broadening my technical knowledge, embracing new challenges along the way so that I can be a better version of myself every day.",
     ],
   },
   technical_skills: {
@@ -30,7 +31,7 @@ const strings = {
     },
   },
   loading: "Loading",
-  loading_message: "I make mobile apps and websites.",
+  loading_message: "I make mobile apps and websites",
 };
 
 export default strings;
