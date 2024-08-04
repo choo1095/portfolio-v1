c0756e2b0be558340b4ac341e59f5d9dd2c9350c
ennichoo <ennichoo@mobius-digital.com>
2024-08-05 00:55:50 +0800

    personal projects: updated contents

--- src/contents/personal_projects.ts
+++ src/contents/personal_projects.ts
@@ -9,8 +9,14 @@ const personalProjects: Project[] = [
     title: "Makan Apa?",
     tags: ["Mobile"],
     tools: ["Flutter"],
+    links: [
+      {
+        title: "GitHub",
+        url: "https://github.com/choo1095/makan",
+      },
+    ],
     description:
-      'I created this Flutter app to solve the age-old dilemma of "eh lunch eat wat?" Using the Google Maps API, it filters and fetches real-time restaurant data to discover the perfect lunch spot. This app was born from a mix of necessity and hunger.',
+      'This app was born from a mix of necessity and hunger. I created this Flutter app to solve the age-old dilemma of "eh lunch eat wat?" Using the Google Maps API, it filters and fetches real-time restaurant data to discover the perfect lunch spot.',
   },
   {
     id: "smart_mirror",
@@ -19,8 +25,14 @@ const personalProjects: Project[] = [
     title: "Gesture Controlled Smart Mirror",
     tags: ["Misc"],
     tools: ["LeapJS", "Javascript"],
+    links: [
+      {
+        title: "Demo Video",
+        url: "https://drive.google.com/file/d/1TfcAZR9ZbonqJ77MpstllnyYrTfgB9ZV/view?usp=sharing",
+      },
+    ],
     description:
-      "I developed a smart mirror using the LeapJS SDK and a Leap Motion Controller. This mirror responds to hand gestures such as circular motions, hovers, pinches, and swipes; and showcases real-time updates, including the current date, time, and weather. It also delivers the latest news, and features a gallery of images and YouTube videos on the mirror's display.",
+      "I developed a smart mirror using the LeapJS SDK and a Leap Motion Controller. This mirror responds to hand gestures such as circular motions, hovers, pinches, and swipes; and showcases various real-time updates such as the current date, time, weather, etc.",
   },
 ];
 
